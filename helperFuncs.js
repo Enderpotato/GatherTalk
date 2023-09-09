1a4621c880a5021f68bc2581b66642abcd7e1ced
Enoch <lim_enoch@students.edu.sg>
2023-09-09 10:48:08 +0800

    Formatting

--- helperFuncs.js
+++ helperFuncs.js
@@ -1,25 +1,53 @@
 function getTimePassed(ms) {
   let seconds = ms / 1000;
 
-  if (seconds < 60) return `${seconds.toFixed()}s ago`;
+  if (seconds < 60) {
+    return `${seconds.toFixed()} s ago`;
+  }
 
   let minutes = seconds / 60;
-  if (minutes < 60) return `${minutes.toFixed()}min ago`;
+  if (minutes < 60) {
+    return `${minutes.toFixed()} min ago`;
+  }
 
   let hours = minutes / 60;
-  if (hours < 24) return `${hours.toFixed()}hrs ago`;
+  if (hours.toFixed() === 1) {
+    return `${hours.toFixed()} hr ago`;
+  }
+  if (hours < 24) {
+    return `${hours.toFixed()} hrs ago`;
+  }
 
   let days = hours / 24;
-  if (days < 7) return `${days.toFixed()}days ago`;
+  if (days.toFixed() === 1) {
+    return `${days.toFixed()} day ago`;
+  }
+  if (days < 7) {
+    return `${days.toFixed()} days ago`;
+  }
 
+  if (weeks.toFixed() === 1) {
+    return `${weeks.toFixed()} wk ago`;
+  }
+  if (weeks < 4) {
+    return `${weeks.toFixed()} wks ago`;
+  }
   let weeks = days / 7;
-  if (weeks < 4) return `${weeks.toFixed()}wks ago`;
 
   let months = days / 30;
-  if (months < 12) return `${months.toFixed()}months ago`;
+  if (months.toFixed() === 1) {
+    return `${months.toFixed()} month ago`;
+  }
+  if (months < 12) {
+    return `${months.toFixed()} months ago`;
+  }
 
   let years = days / 365;
-  return `${years.toFixed()}yrs ago`;
+  let yearsFixed = years.toFixed();
+  if (yearsFixed === 1) {
+    return `${yearsFixed} yr ago`;
+  }
+  return `${years.toFixed()} yrs ago`;
 }
 
 function getTimePassedDiff(postedDate) {
